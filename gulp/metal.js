042fbb542365f42b350008c02893ff1e81ae9bba
Glen Cheney <cheney.glen@gmail.com>
2015-04-19 18:41:01 -0700

    Highlight code with prism

--- gulp/metal.js
+++ gulp/metal.js
@@ -5,11 +5,31 @@ var metalsmith = require('metalsmith');
 var markdown = require('metalsmith-markdown');
 var templates = require('metalsmith-templates');
 var permalinks = require('metalsmith-permalinks');
+var prism = require('prismjs');
+
+// Translate marked languages to prism.
+var extensions = {
+  js: 'javascript',
+  html: 'markup',
+  svg: 'markup',
+  xml: 'markup',
+  py: 'python',
+  rb: 'ruby',
+  ps1: 'powershell',
+  psm1: 'powershell'
+};
 
 module.exports = function(done) {
   metalsmith(path.join(__dirname, '..'))
     .clean(false)
-    .use(markdown())
+    .use(markdown({
+      gfm: true,
+      smartypants: true,
+      highlight: function(code, lang) {
+        lang = extensions[lang];
+        return prism.highlight(code, prism.languages[lang]);
+      }
+    }))
     .use(templates({
       engine: 'swig',
       directory: 'templates'
