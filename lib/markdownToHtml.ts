39486f52669f64e9a42880d279b555b5d7c5ca4f
Glen Cheney <cheney.glen@gmail.com>
2020-06-23 22:01:39 -0700

    Add syntax highlighting to markdown output

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -1,7 +1,20 @@
-import remark from 'remark';
-import html from 'remark-html';
+import MarkdownIt from 'markdown-it';
+import refractor from 'refractor';
+import rehype from 'rehype';
 
-export default async function markdownToHtml(markdown: string) {
-  const result = await remark().use(html).process(markdown);
-  return result.toString();
+const md = MarkdownIt({
+  highlight(str: string, lang: string) {
+    if (lang) {
+      const nodes = refractor.highlight(str, lang);
+      return `<pre class="language-${lang}"><code>${rehype()
+        .stringify({ type: 'root', children: nodes })
+        .toString()}</code></pre>`;
+    }
+
+    return '';
+  },
+});
+
+export default function markdownToHtml(markdown: string) {
+  return md.render(markdown);
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import { AppProps } from 'next/app';
 
+import 'dracula-prism/css/dracula-prism.css';
 import '../styles/index.css';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
